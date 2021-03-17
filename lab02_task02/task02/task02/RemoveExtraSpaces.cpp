55fe87179e27383e44a4ca8e94ab74c9100626b2
Rjomba2000 <61803381+Rjomba2000@users.noreply.github.com>
2021-03-18 00:24:35 +0300

    Add files via upload

---
+++ lab02_task02/task02/task02/RemoveExtraSpaces.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string RemoveExtraSpaces(string const& str)
+{
+	bool firstSpacesDeleted = false;
+	bool spaceWas = false;
+	string resultStr = "";
+
+	for (unsigned i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ' ')
+		{
+			if (firstSpacesDeleted && !spaceWas) 
+			{
+				spaceWas = true;
+			}
+		}
+		else
+		{
+			firstSpacesDeleted = true;
+			if (spaceWas)
+			{
+				resultStr += ' ';
+				spaceWas = false;
+			}
+			resultStr += str[i];
+		}
+	}
+	
+	return resultStr;
+}
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	cout << RemoveExtraSpaces(str);
+}
